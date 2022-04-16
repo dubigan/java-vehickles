ad0ed2b29500384b40c476a47aed52b1f8f26e8c
oak <oak@localhost.localdomain>
2022-04-16 14:56:13 +0300

    initial

---
+++ src/main/java/oak/spring/vehickles/VehicklesApplication.java
@@ -0,0 +1,14 @@
+package oak.spring.vehickles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicklesApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(VehicklesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oak/spring/vehickles/VehicklesApplicationTests.java
@@ -0,0 +1,13 @@
+package oak.spring.vehickles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicklesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
